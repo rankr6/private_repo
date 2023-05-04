b4ce6be4d752f7a6e47f328fcdf838fc7611cba8
rankr66 <125305290+rankr66@users.noreply.github.com>
2023-05-04 11:15:40 +0530

    fixed bugs

--- app.js
+++ app.js
@@ -246,7 +246,7 @@ app.post(
     if (AdminOfSport) {
       response.redirect("/admin/createSport");
     } else {
-      response.redirect("/sportDetail");
+      response.redirect("/login");
     }
   }
 );
