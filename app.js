084f9c74cd75543a5ccc558ead468687f34abc0f
rankr66 <125305290+rankr66@users.noreply.github.com>
2024-01-10 15:44:22 +0530

    updates

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ const { request } = require("http");
 const { log } = require("util");
 const Sequelize = require("sequelize");
 const { Op } = require("sequelize");
+const cors = require("cors")
 //const { next } = require("cheerio/lib/api/traversing");
+app.use(cors())
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
